34d10073954b12fd0845d14facc13525f91be5e2
Marc <marcobrien12@gmail.com>
2013-10-13 16:53:52 -0700

    updates

--- config/boot.rb
+++ config/boot.rb
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 require 'rubygems'
 
-=======
->>>>>>> ecfb22563902877d522e109da995de1ddfd2f1a0
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
